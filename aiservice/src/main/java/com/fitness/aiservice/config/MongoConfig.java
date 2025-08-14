5b33985e1f415840fdea6c899e51c8d4446a5bd7
siddujajee <jajeesiddu2002@gmail.com>
2025-08-15 02:13:24 +0530

    ai service configurations done

---
+++ aiservice/src/main/java/com/fitness/aiservice/config/MongoConfig.java
@@ -0,0 +1,10 @@
+package com.fitness.aiservice.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+// this configuration file is to enable MongoDB auditing features, which allows tracking of entity creation and modification timestamps.
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+
+}
