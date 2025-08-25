bada4efb07c443d3b59c99c1af13e73f48e10084
siddujajee <jajeesiddu2002@gmail.com>
2025-08-26 01:45:49 +0530

    removed few unwanted import statements and centralised configurations of all microservices

--- activityservice/src/main/java/com/fitness/activityservice/config/RabbitMqConfig.java
+++ activityservice/src/main/java/com/fitness/activityservice/config/RabbitMqConfig.java
@@ -9,9 +9,6 @@ import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.rabbitmq.client.AMQP.Exchange;
-
 @Configuration
 public class RabbitMqConfig {
 
